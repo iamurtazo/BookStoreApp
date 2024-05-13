d748e0eb033c9376bcabfad0245648a7174ec1dc
iamurtazo <murtazo207@inha.edu>
2024-05-13 22:57:35 +0500

    Category CRUD Operations and Views

---
+++ BookStoreApp/Controllers/CategoryController.cs
@@ -0,0 +1,108 @@
+﻿using BookStoreApp.Data;
+using BookStoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    #region Index
+    public IActionResult Index()
+    {
+        List<Category> categories = _context.Categories.ToList();
+        return View(categories);
+    }
+    #endregion
+
+    #region Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category category) 
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    #endregion
+
+    #region Edit
+    public IActionResult Edit(int? id)
+    {
+        Category category = _context.Categories.Find(id);
+
+        if (category == null)
+            return NotFound();
+
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(int id, Category category)
+    {
+        if(id != category.Id)
+            return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+    #endregion
+
+    #region Details
+    public IActionResult Details(int? id)
+    {
+        Category category = _context.Categories.Find(id);
+
+        if (category == null)
+            return NotFound();
+
+        return View(category);
+    }
+    #endregion
+
+
+    #region Delete
+    public IActionResult Delete(int? id)
+    {
+        Category category = _context.Categories.Find(id);
+
+        if (category == null)
+            return NotFound();
+
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int? id)
+    {
+        Category category = _context.Categories.Find(id);
+
+        if(category == null)
+            return NotFound();
+
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    #endregion
+}
---
+++ BookStoreApp/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.Models;
+
+public class Category
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    [DisplayName("Category Name")]
+    public string Name { get; set; }
+    [Required]
+    [DisplayName("Display Order")]
+    [Range(1, int.MaxValue, ErrorMessage = "Number must be greater than 0")]
+    public int DisplayOrder { get; set; }
+}
